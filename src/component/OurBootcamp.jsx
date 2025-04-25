4b5c500ef3bb98d831745d0871c5239ffd3932f9
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-04-25 15:05:59 +0000

    implements mobile navigation

--- src/component/OurBootcamp.jsx
+++ src/component/OurBootcamp.jsx
@@ -4,24 +4,24 @@ import apply from "/apply.svg"
 export const OurBootcamp = () => {
   return (
     <div
-            class="w-[90%] mb-28 p-4 flex flex-col items-center md:mx-auto  space-y-10 space-x-10"
+            className="w-[90%] mb-28 p-4 flex flex-col items-center md:mx-auto  space-y-10 space-x-10"
           >
             <div>
-              <h2 class="text-2xl font-bold">Our Bootcamps</h2>
+              <h2 className="text-2xl font-bold">Our Bootcamps</h2>
             </div>
 
             <div
-              class="w-full flex flex-col md:flex-row  space-x-10 space-y-10 items-end"
+              className="w-full flex flex-col md:flex-row  space-x-10 space-y-10 items-end"
             >
               <div
-                class="w-full mx-auto md:w-1/2 border-2 border-[#3f1c4a] p-6 flex flex-col"
+                className="w-full mx-auto md:w-1/2 border-2 border-[#3f1c4a] p-6 flex flex-col"
               >
                 <div
-                  class="bg-web2 h-[250px]  flex items-center justify-center mb-8"
+                  className="bg-web2 h-[250px]  flex items-center justify-center mb-8"
                 >
-                  <h2 class="text-2xl text-center">Web2 for Web3 Preparatory Bootcamp</h2>
+                  <h2 className="text-2xl text-center">Web2 for Web3 Preparatory Bootcamp</h2>
                 </div>
-                <p class="tracking-wider leading-loose">
+                <p className="tracking-wider leading-loose">
                   Learn the essentials of modern web development in HTML, CSS,
                   Tailwind CSS, JavaScript, and Node.js/React. This program
                   guides you through creating interactive, responsive websites,
@@ -30,25 +30,25 @@ export const OurBootcamp = () => {
                   their web journey!
                 </p>
                 <button
-                  class="mb-2 self-center border-2 border-[#3f1c4a] flex items-center gap-3 py-3 px-8 bg-[#ab65ef] mt-8 md:mt-36"
+                  className="mb-2 self-center border-2 border-[#3f1c4a] flex items-center gap-3 py-3 px-8 bg-[#ab65ef] mt-8 md:mt-36"
                 >
                   Join the Waitlist
                   <img src={apply} alt="" />
                 </button>
-                <p class="italic text-[#107c9f] self-center">
+                <p className="italic text-[#107c9f] self-center">
                   NOTE: Registration for Cohort III will start on March 1st.
                 </p>
               </div>
 
               <div
-                class="w-full mx-auto md:w-1/2 border-2 border-[#3f1c4a] p-6  flex flex-col "
+                className="w-full mx-auto md:w-1/2 border-2 border-[#3f1c4a] p-6  flex flex-col "
               >
                 <div
-                  class="bg-web3 h-[250px] flex items-center justify-center mb-8"S
+                  className="bg-web3 h-[250px] flex items-center justify-center mb-8"S
                 >
-                  <h2 class="text-2xl">Web3 Bootcamp</h2>
+                  <h2 className="text-2xl">Web3 Bootcamp</h2>
                 </div>
-                <p class="tracking-wider leading-loose">
+                <p className="tracking-wider leading-loose">
                   Unlock the future of the internet with our Web3 Bootcamp!
                   Learn blockchain fundamentals, smart contracts, and dApps to
                   build decentralized applications. Ideal for beginners eager to
@@ -56,12 +56,12 @@ export const OurBootcamp = () => {
                   blockchain development!
                 </p>
                 <button
-                  class="mb-2 self-center border-2 border-[#3f1c4a] flex items-center gap-3 py-3 px-8 bg-[#ab65ef] mt-8 md:mt-36"
+                  className="mb-2 self-center border-2 border-[#3f1c4a] flex items-center gap-3 py-3 px-8 bg-[#ab65ef] mt-8 md:mt-36"
                 >
                   Join the Waitlist
                   <img src={apply} alt="" />
                 </button>
-                <p class="italic text-[#107c9f] self-center">
+                <p className="italic text-[#107c9f] self-center">
                   NOTE: Registration for Cohort III will start on March 1st.
                 </p>
               </div>
--- src/component/sharedcomponent/Navbar.jsx
+++ src/component/sharedcomponent/Navbar.jsx
@@ -1,26 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import logo from "/blockfuselogo1.png";
 import { NavLink } from "react-router-dom";
 import apply from "/apply.svg";
 import sun from "/sun.svg";
 import moon from "/moon.svg";
+import hamburger from "/hamburger.svg"
 import { themeContextProvider } from "../../Contexts/ThemeContext";
+import closebtn from "/closebtn.svg"
 
 const Navbar = () => {
+  const [isopen, setIsOpen] = useState(false)
+  const [src, setSrc] = useState()
   const { darkTheme, toggleTheme } = useContext(themeContextProvider);
 
+  const toggleSideMenu = ()=> {
+    setIsOpen(prev => !prev)
+  }
+
   return (
+    <>
     <div
       className={
         darkTheme
-          ? "bg-[#2f2e34] md:w-full xl:w-[90%] 2xl:w-4/5 py-4 px-8 mx-auto mt-8 mb-24 md:mb-36 flex items-center justify-between text-white relative"
-          : " w-4/5 py-4 px-8 mx-auto mt-8 mb-24 md:mb-36 flex items-center justify-between  relative border-[1px]"
+          ? "bg-[#2f2e34] w-full xl:w-[90%] 2xl:w-4/5 py-4 px-8 mx-auto mt-8 mb-24 md:mb-36 flex items-center justify-between text-white relative"
+          : " w-full xl:w-[90%] 2xl:w-4/5 py-4 px-8 mx-auto mt-8 mb-24 md:mb-36 flex items-center justify-between  relative border-[1px]"
       }
     >
       <div>
         <img src={logo} alt="" width="32" height="32" />
       </div>
-      <div className=" hidden md:flex md:items-center space-x-4 opacity-70">
+      <div className=" hidden md:flex md:items-center space-x-4 opacity-70 ">
         <NavLink to="/">
           {({ isActive }) => (
             <span className={isActive ? "active" : ""}>Home</span>
@@ -36,6 +45,7 @@ const Navbar = () => {
         <NavLink to="contactus">Contact Us</NavLink>
         <NavLink to="faucet">Faucet</NavLink>
       </div>
+     
       <div className="flex items-center space-x-4">
         <button
           className={
@@ -50,19 +60,45 @@ const Navbar = () => {
           Apply Now
           <img src={apply} alt="apply image" />
         </button>
-        <div>
+        <div className="flex items-center gap-3">
           <img
-            src={darkTheme?sun:moon}
+            src={darkTheme ? sun : moon}
             alt="theme changer"
             onClick={toggleTheme}
             width={24}
             height={24}
           />
+          <img
+            src={isopen? closebtn: hamburger}
+            alt=""
+            className="md:hidden"
+            onClick={toggleSideMenu}
+          />
         </div>
       </div>
-
-      
+     
     </div>
+    {isopen && <div className={darkTheme?"mobileMenu bg-[#2f2e34;]": "mobileMenu bg-white border-2"}>
+        <NavLink to="/">
+          {({ isActive }) => (
+            <span className={isActive ? "active" : ""}>Home</span>
+          )}
+        </NavLink>
+        <NavLink to="/aboutus">About Us</NavLink>
+        <NavLink to="/team">Team</NavLink>
+        <NavLink to="bootcamp">Bootcamp</NavLink>
+        <NavLink to="events">Event</NavLink>
+        <NavLink to="alumni">Alumni</NavLink>
+        <NavLink to="blog">Blog</NavLink>
+        <NavLink to="opensource">Open Source</NavLink>
+        <NavLink to="contactus">Contact Us</NavLink>
+        <NavLink to="faucet">Faucet</NavLink>
+        <button className="text-white flex gap-3 items-center py-2 px-8  w-fit bg-[#ab65ef]">
+          Apply Now
+          <img src={apply} alt="apply image" />
+        </button>
+      </div>}
+    </>
   );
 };
 
